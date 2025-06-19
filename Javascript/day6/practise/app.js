ebad2c7160f70f5bd39d3c6ce35b11fe036c2617
ballur raghavendra raju <raghav0611@gmail.com>
2025-06-19 17:07:40 +0530

    day 6 practise

---
+++ Javascript/day6/practise/app.js
@@ -0,0 +1,15 @@
+console.log("Hello Promise")
+
+const successCallbackFn = (data) => {
+    console.log("Data downloaded.", data)
+}
+
+const failureCallbackFn = (err) => {
+    console.log("Data download failed.",err)
+}
+const recipes_promise = fetch(input="https://dummyjson.collm/recipes")
+
+console.log(recipes_promise);
+
+recipes_promise.then(successCallbackFn).catch(failureCallbackFn)
+
