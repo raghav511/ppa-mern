3f9a61424e931716210a01529245d4a8467c962c
Likhilesh <likbalpande@example.com>
2025-06-25 23:19:40 +0530

    react Components import _ export, css

--- React/day3/main.js
+++ React/day3/main.js
@@ -1,6 +1,12 @@
 import React from "react"; // if its user defined, is it internal nodejs, node_modules
 import ReactDOM from "react-dom/client";
 
+import "./main.css";
+
+import Header from "./components/Header"; // default import
+import Footer from "./components/Footer"; // default import
+import { Button, Main } from "./components/Main"; // named import
+
 const domRoot = document.getElementById("root");
 const reactRoot = ReactDOM.createRoot(domRoot);
 
@@ -8,7 +14,10 @@ const App = () => {
   console.log("Likhilesh");
   return (
     <div>
-      <h1>Hello from React App!!</h1>
+      <Header />
+      <Main />
+      <Footer />
+      <Button />
     </div>
   );
 };
