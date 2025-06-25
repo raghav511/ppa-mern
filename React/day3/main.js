99433c34c9254fc9464cae36518f6eeefb8301ee
Likhilesh <likbalpande@example.com>
2025-06-25 22:42:26 +0530

    React App - Parcel (from Scratch)

--- React/day3/main.js
+++ React/day3/main.js
@@ -1,26 +1,16 @@
 import React from "react"; // if its user defined, is it internal nodejs, node_modules
 import ReactDOM from "react-dom/client";
 
-import "./main.css";
-
-import Header from "./components/Header"; // default import
-import Footer from "./components/Footer"; // default import
-import { Button, Main } from "./components/Main"; // named import
-
 const domRoot = document.getElementById("root");
 const reactRoot = ReactDOM.createRoot(domRoot);
 
 const App = () => {
-    console.log("Likhilesh");
-    return (
-        <div>
-            <Header />
-            <Main />
-            <Footer />
-            <Button />
-        </div>
-    );
+  console.log("Likhilesh");
+  return (
+    <div>
+      <h1>Hello from React App!!</h1>
+    </div>
+  );
 };
 
-// reactRoot.render(App());
-reactRoot.render(<App />);
+reactRoot.render(App());
