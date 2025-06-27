34d77be79923938db34f7823a8721c43d5409eb2
ballur raghavendra raju <raghav0611@gmail.com>
2025-06-27 21:33:10 +0530

    react day3 practise

---
+++ React/day 3 practise node js and bundler/app.js
@@ -0,0 +1,23 @@
+import React from "react"; // if its user defined, is it internal nodejs, node_modules
+import ReactDOM from "react-dom/client";
+
+import Header from "./components/Header"; // default import
+import Footer from "./components/Footer"; // default import
+import { Button, Main } from "./components/Main"; // named import
+
+const domRoot = document.getElementById("root");
+const reactRoot = ReactDOM.createRoot(domRoot);
+
+const App = () => {
+  console.log("Likhilesh");
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+      <Button />
+    </div>
+  );
+};
+
+reactRoot.render(App());
