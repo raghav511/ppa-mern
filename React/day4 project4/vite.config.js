9c5ebd8fcf67aa9a5ceb9dda159e52324fd1273d
ballur raghavendra raju <raghav0611@gmail.com>
2025-06-29 14:58:28 +0530

    add gh pages

--- React/day4 project4/vite.config.js
+++ React/day4 project4/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ppa-mern/calculator",
+  base: "/ppa-mern/React/day4%20project4",
 });
