9e5ecae1a26fce2519668c44e6f1264d94ff59a5
Likhilesh <likbalpande@example.com>
2025-06-30 22:54:27 +0530

    useState - non-primitive : arrays, objects.

--- React/day6/src/App.jsx
+++ React/day6/src/App.jsx
@@ -1,40 +1,52 @@
-// un-controlled component -- REACT
+import { useState } from "react";
+
 const App = () => {
-    const handleSubmit = (e) => {
+    const [fruits, setFruits] = useState(() => {
+        const savedFruits = localStorage.getItem("fruits"); // text (string) (JSON)
+        if (savedFruits) {
+            const arr = JSON.parse(savedFruits); // convert string (JSON) --> Array/Object :: JSON.parse
+            return arr;
+        } else {
+            return [];
+        }
+    });
+
+    const handleAddFruit = (e) => {
         e.preventDefault();
 
-        const name = e.target.username.value;
-        const city = e.target.city.value;
-        const email = e.target.email.value;
+        const fruitName = e.target.fruitName.value;
 
-        if (name.length <= 2) {
-            alert("Invalid name");
-        } else if (city.length <= 1) {
-            alert("Invalid city");
-        } else if (email.length < 5) {
-            alert("Invalid email");
-        } else {
-            alert("success!");
-        }
+        setFruits((prev) => {
+            const newArr = [...prev];
+
+            newArr.push(fruitName);
+
+            localStorage.setItem("fruits", JSON.stringify(newArr)); // convert Array/Object --> string (JSON) :: JSON.stringify
+
+            return newArr;
+        });
     };
 
     return (
-        <>
-            {/* We were getting the value from a event but the control was always with DOM */}
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <input type="text" name="username" required></input>
-                </div>
-                <div>
-                    <input type="email" name="email" required></input>
-                </div>
-                <div>
-                    <input type="text" name="city" required></input>
-                </div>
-                <button>Add User</button>
+        <div>
+            <form onSubmit={handleAddFruit}>
+                <input type="text" placeholder="fruit name" name="fruitName" required />
+                <br />
+                <br />
+                <button>Submit</button>
             </form>
-            <div></div>
-        </>
+            <div>
+                {fruits.map((elem) => {
+                    return (
+                        <div key={elem}>
+                            <p>{elem}</p>
+                            <button>Edit</button>
+                            <button>Delete</button>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
     );
 };
 
