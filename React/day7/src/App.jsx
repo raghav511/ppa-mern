b0f3678e5eb6e438a6a4cd1eff15f00b4bb5e305
Likhilesh <likbalpande@example.com>
2025-07-01 23:48:56 +0530

    then-catch, async-await, useEffect, API call

--- React/day7/src/App.jsx
+++ React/day7/src/App.jsx
@@ -1,24 +1,51 @@
 import { useEffect } from "react";
 import { useState } from "react";
 
+// re-render : re-run
 const App = () => {
-    const [count, setCount] = useState(0);
+    console.log("A");
+    const [recipesArr, setRecipesArr] = useState([]); // hook: special function given to us by react *
+
+    console.log("B");
+    const getData = async () => {
+        try {
+            console.log("H");
+            const response = await fetch("https://dummyjson.com/recipes");
+            const data = await response.json();
+            console.log("G", data.recipes);
+            setRecipesArr(data.recipes); // [] --> [...] !noted
+        } catch (err) {
+            alert(`Api Error: ${err.message}`);
+        }
+    };
+    console.log("C");
+
+    // useEffect(() => {
+    //     // getData();
+    // }); // 1. when you don't mention any dependency array --> the callback function will run every-time the component re-renders
 
     useEffect(() => {
-        console.log("Hello");
+        console.log("D");
+        getData();
+        console.log("E");
+    }, []); // 2. when you mention EMPTY dependency array --> the callback function will run only on "INITIAL RENDER"
 
-        return () => {
-            console.log("Goodbye", count);
-        };
-    });
+    // setTimeout(() => {
+    //     getData();
+    // }, 10000);
 
-    console.log("... component rendered or re-rendered", count);
-
+    console.log("F");
     return (
-        <div className="App">
-            <h1>{count}</h1>
-            <button onClick={() => setCount(count + 1)}>Increment</button>
-            {console.log("done...")}
+        <div>
+            <h1>RECIPES...</h1>
+            {recipesArr.map((elem) => {
+                return (
+                    <div key={elem.id}>
+                        <h4>{elem.name}</h4>
+                        <p>{elem.cuisine}</p>
+                    </div>
+                );
+            })}
         </div>
     );
 };
