5f19b20b345a02e5089fdcc6b24d6828cb84f22e
Likhilesh <likbalpande@example.com>
2025-07-01 20:54:23 +0530

    useState CRUD Operations & introduction to useEffect

--- React/day7/src/App.jsx
+++ React/day7/src/App.jsx
@@ -1,53 +1,26 @@
 import { useEffect } from "react";
 import { useState } from "react";
 
-// re-render : re-run
 const App = () => {
-    console.log("A");
-    const [recipesArr, setRecipesArr] = useState([]); // hook: special function given to us by react *
+  const [count, setCount] = useState(0);
 
-    console.log("B");
-    const getData = async () => {
-        try {
-            console.log("H");
-            const response = await fetch("https://dummyjson.com/recipes");
-            const data = await response.json();
-            console.log("G", data.recipes);
-            setRecipesArr(data.recipes); // [] --> [...] !noted
-        } catch (err) {
-            alert(`Api Error: ${err.message}`);
-        }
+  useEffect(() => {
+    console.log("Hello");
+
+    return () => {
+      console.log("Goodbye", count);
     };
-    console.log("C");
+  });
 
-    // useEffect(() => {
-    //     // getData();
-    // }); // 1. when you don't mention any dependency array --> the callback function will run every-time the component re-renders
+  console.log("... component rendered or re-rendered", count);
 
-    useEffect(() => {
-        console.log("D");
-        getData();
-        console.log("E");
-    }, []); // 2. when you mention EMPTY dependency array --> the callback function will run only on "INITIAL RENDER"
-
-    // setTimeout(() => {
-    //     getData();
-    // }, 10000);
-
-    console.log("F");
-    return (
-        <div>
-            <h1>RECIPES...</h1>
-            {recipesArr.map((elem) => {
-                return (
-                    <div key={elem.id}>
-                        <h4>{elem.name}</h4>
-                        <p>{elem.cuisine}</p>
-                    </div>
-                );
-            })}
-        </div>
-    );
+  return (
+    <div className="App">
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      {console.log("done...")}
+    </div>
+  );
 };
 
 export default App;
