c30d28ebe17dcaa841a94c091cf084ee577a9391
Likhilesh <likbalpande@example.com>
2025-07-08 23:16:31 +0530

    IIFE vs function assignment

--- Nodejs/day1/working-with-fs-promises.js
+++ Nodejs/day1/working-with-fs-promises.js
@@ -1,11 +1,21 @@
 const fsPromises = require("fs/promises");
 
 console.log("START");
+// function declaration --> function abcd(){}
+// (named) function assignment --> const xyz = function abcd(){}
+// (anonymous) function assignment --> const xyz = function (){}
+// (arrow) function assignment --> const xyz =  ()=>{}
+// IIFE
 
 const main = async () => {
     const data = await fsPromises.readFile("./my-file.txt", "utf-8");
     console.log(data);
 };
 main();
+
+// (async () => {
+//     const data = await fsPromises.readFile("./my-file.txt", "utf-8");
+//     console.log(data);
+// })();
 
 console.log("END");
